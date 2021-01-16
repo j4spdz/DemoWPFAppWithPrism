496fa55fa2f84801533cfb3243ee45b32627a363
j4spdz <code.jack4spadesz@gmail.com>
2021-01-16 11:26:19 +0800

    Add project files.

---
+++ src/DemoApp.Core/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DemoApp.Core.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoApp.Core
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        //This function is used to register any app dependencies
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
